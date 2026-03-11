a8d54c3c3881bae25eec9fe597df18a9a15707dc
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2026-03-11 16:25:41 +0200

    1

--- index.php
+++ index.php
@@ -820,6 +820,17 @@
             '”. Un consultant îți va trimite oferta pe email în cel mai scurt timp.';
     }
 </script>
-
+<!-- Script live chat Tawk.to -->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/XXXXXXXXXX/XXXXXXXXXX'; // înlocuiești cu linkul tău
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
 </body>
 </html>
